d16e9484496c1ef708607acbd16f3a5407b18834
sina s3h <sinas3h@gmail.com>
2023-03-12 23:55:14 +0330

    change something

--- src/component/MainPage.js
+++ src/component/MainPage.js
@@ -10,15 +10,21 @@ const MainPage = () => {
 
     const products = useContext(ProductContext)
 
+
     return (
         <div className={style.mainWrapper}>
 
         <div className={style.mainDiv}>
-            <h2 className={style.header}>Shop</h2>
+            <div>
+                <h3>Home/Products</h3>
+                <h2 className={style.header}>Shop</h2>
+                <h3>default sorting</h3>
+            </div>
         </div>
 
         <div className={style.main}>
-            {products.map(product => <CartShop key={product.id} productData={product} />)}
+            {!products.length ? <div><span className={style.loading}></span></div> :
+            products.map(product => <CartShop key={product.id} productData={product} />)}
         </div>
 
         </div>
